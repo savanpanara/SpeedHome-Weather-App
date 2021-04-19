d410a3de2475916b5476db58153e140b503abdad
savanpanara <panarasavan@gmail.com>
2021-04-19 10:27:28 +0530

    local notification created

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import Navigator from '@src/navigator';
 import {colors} from '@src/theme';
+import {LocalNotification} from '@src/utils/pushNotification';
 
 const App = () => {
+  useEffect(() => {
+    LocalNotification();
+  }, []);
+
   return (
     <>
       <StatusBar
