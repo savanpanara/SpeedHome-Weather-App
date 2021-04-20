137cff97d7ab3697658892a1ef5d53cc926fd88c
savanpanara <panarasavan@gmail.com>
2021-04-20 12:11:21 +0530

    added geolocation

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import React, {useEffect} from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, PermissionsAndroid, Alert} from 'react-native';
 import Navigator from '@src/navigator';
 import {colors} from '@src/theme';
 import {LocalNotification} from '@src/utils/pushNotification';
 import PushNotification from 'react-native-push-notification';
+import Geolocation from 'react-native-geolocation-service';
 
 const App = () => {
   useEffect(() => {
     //remove previous notification
     PushNotification.removeAllDeliveredNotifications();
     LocalNotification();
-  }, []);
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(position);
+      },
+      error => {
+        Alert.alert(`Code ${error.code}`, error.message);
+        console.log(error);
+      },
+      {
+        accuracy: {
+          android: 'high',
+          ios: 'best',
+        },
 
+        timeout: 15000,
+        maximumAge: 10000,
+        distanceFilter: 0,
+      },
+    );
+  }, []);
   return (
     <>
       <StatusBar
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,16 +1,46 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Box, Text} from '@components';
+import {ActivityIndicator} from 'react-native';
 import {connect} from 'react-redux';
 import {getCities} from '@src/store/actions';
 import List from './widget/List';
+import {colors} from '../../theme';
 const HomeScreen = ({getCities, navigation}) => {
+  const [isLoading, setLoading] = useState(true);
   useEffect(() => {
-    getCities();
+    const func = async () => {
+      try {
+        getCities();
+      } catch (e) {
+        alert('error while get cities');
+      } finally {
+        setLoading(false);
+      }
+    };
+    func();
   }, []);
+
+  const Loader = () => {
+    return (
+      <Box
+        backgroundColor={colors.white}
+        justifyContent={'center'}
+        flex={1}
+        alignItems={'center'}>
+        <ActivityIndicator size="large" color={colors.black} />
+      </Box>
+    );
+  };
   return (
-    <Box>
-      <List navigation={navigation} />
-    </Box>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Box>
+          <List navigation={navigation} />
+        </Box>
+      )}
+    </>
   );
 };
 export default connect(null, {getCities})(HomeScreen);
